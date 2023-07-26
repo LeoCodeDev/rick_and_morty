f34d3bf8fc258958a7b715bde42e3a331041c06a
LeoCodeDev <alb.mar.cta@gmail.com>
2023-07-26 17:48:00 -0500

    Fix: bug de personajes en favorites fixed

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -8,9 +8,9 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FAV:
-      return {...state, myFavorites: action.payload, allCharacters: action.payload}
+      return {myFavorites: action.payload, allCharacters: action.payload}
     case REMOVE_FAV:
-      return {...state, myFavorites: action.payload}
+      return {myFavorites: action.payload, allCharacters:action.payload}
     case FILTER:
       if (action.payload === "all")
         return { ...state, myFavorites: [...state.allCharacters] };
