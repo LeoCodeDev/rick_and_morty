661b64d25ca3be90853b4d3921116cc4945cbf5d
LeoCodeDev <alb.mar.cta@gmail.com>
2023-07-27 08:42:34 -0500

    Replanteando funcionamiento de handleScroll

--- client/src/components/cursorShip/CursorShip.jsx
+++ client/src/components/cursorShip/CursorShip.jsx
@@ -8,18 +8,20 @@ const CursorShip = () => {
   const handleMouseMove = (event) => {
     setPosition({ x: event.pageX + 15, y: event.pageY + 15 });
   };
-
-  const handleScroll = () => {
-    setPosition({ x: 0, y: 0 });
+  
+  const handleScroll = (event) => {
+    // ? aqui hay que colocar una propiedad que mueva la nave de regreso al pointer
+    // TODO Investigar cual es el evento y la propiedad correcta para ese caso
+    // ! Actualmente usamos scroll pero no tiene propiedad de posiciones.
   };
 
   React.useEffect(() => {
     document.addEventListener("mousemove", handleMouseMove);
-    // document.addEventListener("scroll", handleScroll);
+    document.addEventListener("scroll", handleScroll);
 
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
-      // document.removeEventListener("scroll", handleScroll);
+      // document.removeEventListener("scroll", (e)=>{handleScroll(e)});
     };
   }, []);
 
