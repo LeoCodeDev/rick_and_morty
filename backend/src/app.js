b0dba8e082c43c8471112b7bf3637507725b7402
LeoCodeDev <alb.mar.cta@gmail.com>
2023-10-21 23:00:27 -0500

    agregando BASE_URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,11 @@ const router = require("./routes");
 const morgan = require("morgan");
 const cors = require('cors');
 const server = express()
+const origin = process.env.BASE_URL || `http://localhost:3000`
 
 server.use(morgan("dev"));
 
-server.use(cors())
+server.use(cors({origin: origin}))
 
 server.use(express.json())
 
